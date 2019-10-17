28850b34be224f16c3f25b2baa9762fbec7fb9b1
Ilya Lebedev <bacherko@gmail.com>
2019-10-17 19:54:22 +0300

    feat: add methods into "list" contract

--- src/actions/list.js
+++ src/actions/list.js
@@ -2,5 +2,5 @@ import Basic from './basic';
 
 export default class List extends Basic {
   _contractAccount = 'comn.list';
-  _contractActions = ['create', 'setsysparams', 'setinfo'];
+  _contractActions = ['create', 'setsysparams', 'setparams', 'setinfo', 'follow', 'unfollow', 'ban', 'unban'];
 }
